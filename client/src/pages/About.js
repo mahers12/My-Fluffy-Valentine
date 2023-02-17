fcddf180e048100e3c3f7c7072b7922ad777cc48
Sarah Maher <119453789+mahers12@users.noreply.github.com>
2023-02-17 06:31:13 -0600

    added content to about page and styled it

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,8 +1,28 @@
 const About = () => {
   return (
     <div className="flex-col">
-      <h1> About Page</h1>
-      <span className="about">Information about the site</span>
+      <h1> Reasons to Adopt</h1>
+      <span className="about">
+        <ul>
+          <li>
+            Adoption Saves Lives Each year, over 7 million dogs and cats enter
+            shelters nationwide, and nearly 3 million don’t make it out alive.
+            It doesn’t have to be that way. An estimated 17 million people will
+            add a new pet to their families this year. If more of them would
+            choose to adopt a pet, instead of buying one from a pet store,
+            breeder or online, we could significantly reduce the number of
+            animals killed in shelters.
+          </li>
+          <li>Because you'll help a pet in need.</li>
+          <li>Because you'll find a great addition to your family.</li>
+          <li>Because it’ll cost less.</li>
+          <li>
+            You'll encourage others to adopt from shelters when they see how
+            awesome your own pet is.
+          </li>
+          <li>Because it's one way to fight puppy mills.</li>
+        </ul>
+      </span>
     </div>
   )
 }
